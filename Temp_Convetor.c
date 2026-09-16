e81b55e05c0669d6a02dd3f8236403298db323ca
SHYAM SUNDAR K <shyam524561@google.com>
2026-09-16 19:24:50 +0530

    Temperature Convetor

---
+++ Temp_Convetor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float fahrenheit, celsius;
+
+    
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+
+    
+    celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+
+    
+    printf("%.2f Fahrenheit is equal to %.2f Celsius\n", fahrenheit, celsius);
+
+    return 0;
+}
